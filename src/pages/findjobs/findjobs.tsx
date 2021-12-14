49e9bee0cef5c842f9350c5970a281b677c81871
mina <mina@dfrc.ch>
2021-12-15 01:05:12 +0900

    Fetch jobs data added

--- src/pages/findjobs/findjobs.tsx
+++ src/pages/findjobs/findjobs.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { JobType } from "../../types/types";
 import styles from "./findjobs.module.scss";
 
 function FindJobs() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState<JobType[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    try {
+      fetch("https://my-project-c37fd-default-rtdb.firebaseio.com/jobpost.json")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const jobList = [];
+          for (const key in data) {
+            const job = {
+              id: key,
+              ...data[key],
+            };
+            jobList.push(job);
+          }
+
+          setIsLoading(false);
+          setJobs(jobList);
+          console.log("jobs", jobs);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  if (isLoading) {
+    <section>
+      <p>Loading</p>
+    </section>;
+  }
   return (
     <div className={styles.FindJobs}>
       <h1>FindJobs</h1>
