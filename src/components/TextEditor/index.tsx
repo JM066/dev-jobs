cb6a110fe1e069f4ed6fff8cc521d33e53afddf8
Jungmin Lee <mina.lee066@gmail.com>
2022-04-25 18:10:53 +0900

    Draftjs added and in progress

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,177 +1,69 @@
-import React, { useCallback, useMemo, useState } from "react";
-import { Button } from "@chakra-ui/react";
-import {
-  BaseEditor,
-  createEditor,
-  Descendant,
-  Editor,
-  // Element as SlateElement,
-  // Node,
-  // Text,
-  // Transforms,
-} from "slate";
-import { Slate, Editable, ReactEditor, withReact, useSlate } from "slate-react";
-import { HistoryEditor, withHistory } from "slate-history";
-
-export type ParagraphElement = {
-  type: "paragraph";
-  children: CustomText[];
-};
-
-export type TitleElement = {
-  type: "title";
-  children: CustomText[];
-};
-
-type CustomEditor = BaseEditor & ReactEditor & HistoryEditor;
-type CustomElement = ParagraphElement | TitleElement;
-type CustomText = { text: string; bold?: boolean };
-
-declare module "slate" {
-  interface CustomTypes {
-    Editor: CustomEditor;
-    Element: CustomElement;
-    Text: CustomText;
-  }
-}
-
-const initialValue: Descendant[] = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
+import React from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
+const BLOCK_TYPES = [
+  { label: "H1", style: "header-one" },
+  { label: "H2", style: "header-two" },
+  { label: "H3", style: "header-three" },
+  { label: "H4", style: "header-four" },
+  { label: "H5", style: "header-five" },
+  { label: "H6", style: "header-six" },
+  { label: "Blockquote", style: "blockquote" },
+  { label: "UL", style: "unordered-list-item" },
+  { label: "OL", style: "ordered-list-item" },
+  { label: "Code Block", style: "code-block" },
 ];
+
 function TextEditor() {
-  const [value] = useState(initialValue);
+  const [editorState, setEditorState] = React.useState(
+    EditorState.createEmpty()
+  );
 
-  const renderElement = useCallback((props) => <Element {...props} />, []);
-  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const editorRef = React.useRef<any>(null);
+
+  function focusEditor() {
+    if (editorRef.current !== null) {
+      editorRef?.current?.focus();
+    }
+  }
+
+  React.useEffect(() => {
+    focusEditor();
+  }, []);
+
+  const onBlockClick = (style: string) => {
+    const nextState = RichUtils.toggleBlockType(editorState, style);
+    console.log("nextState", nextState);
+    setEditorState(nextState);
+  };
+
+  interface Props {
+    key: string;
+    label: string;
+    onToggle: (style: string) => void;
+    style: string;
+  }
+  const StyleButton = ({ label, style, onToggle }: Props) => {
+    return <button onClick={() => onToggle(style)}>{label}</button>;
+  };
   return (
-    <Slate editor={editor} value={value}>
-      <MarkButton format="bold" />
-      <MarkButton format="italic" />
-      <MarkButton format="underline" />
-      <MarkButton format="code" />
-
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        placeholder="Enter some rich text…"
-        spellCheck
-        autoFocus
-        onKeyDown={(event) => {
-          // for (const hotkey in HOTKEYS) {
-          //   if (isHotkey(hotkey, event as any)) {
-          //     event.preventDefault();
-          //     const mark = HOTKEYS[hotkey];
-          //     toggleMark(editor, mark);
-          //   }
-          // }
-        }}
+    <div onClick={focusEditor}>
+      <div>
+        {BLOCK_TYPES.map((type) => (
+          <StyleButton
+            key={type.label}
+            label={type.label}
+            onToggle={onBlockClick}
+            style={type.style}
+          />
+        ))}
+      </div>
+      <Editor
+        ref={(editor: any) => (editorRef.current = editor)}
+        editorState={editorState}
+        onChange={(editorState) => setEditorState(editorState)}
       />
-    </Slate>
+    </div>
   );
 }
+
 export default TextEditor;
-
-const isMarkActive = (editor: CustomEditor, format: string) => {
-  const marks = Editor.marks(editor);
-  return marks ? true : false;
-};
-const toggleMark = (editor: CustomEditor, format: string) => {
-  console.log("editor,", editor);
-  const isActive = isMarkActive(editor, format);
-
-  if (isActive) {
-    Editor.removeMark(editor, format);
-  } else {
-    Editor.addMark(editor, format, true);
-  }
-
-  // Transforms.setNodes(
-  //   editor,
-  //   { bold: isActive ? undefined : true },
-  //   { match: (n) => Text.isText(n), split: true }
-  // );
-};
-
-const MarkButton = ({ format }: { format: string }) => {
-  const editor = useSlate();
-  return (
-    <Button
-      colorScheme="teal"
-      variant="outline"
-      onMouseDown={(event) => {
-        event.preventDefault();
-        toggleMark(editor, format);
-      }}
-    >
-      Mark button
-    </Button>
-  );
-};
-
-const Element = ({ attributes, children, element }: any) => {
-  const style = { textAlign: element.align };
-  switch (element.type) {
-    case "block-quote":
-      return (
-        <blockquote style={style} {...attributes}>
-          {children}
-        </blockquote>
-      );
-    case "bulleted-list":
-      return (
-        <ul style={style} {...attributes}>
-          {children}
-        </ul>
-      );
-    case "heading-one":
-      return (
-        <h1 style={style} {...attributes}>
-          {children}
-        </h1>
-      );
-    case "heading-two":
-      return (
-        <h2 style={style} {...attributes}>
-          {children}
-        </h2>
-      );
-    case "list-item":
-      return (
-        <li style={style} {...attributes}>
-          {children}
-        </li>
-      );
-    case "numbered-list":
-      return (
-        <ol style={style} {...attributes}>
-          {children}
-        </ol>
-      );
-    default:
-      return (
-        <p style={style} {...attributes}>
-          {children}
-        </p>
-      );
-  }
-};
-const Leaf = ({ attributes, children, leaf }: any) => {
-  console.log("leaf", leaf);
-  if (leaf.bold) {
-    children = <strong>{children}</strong>;
-  }
-  if (leaf.code) {
-    children = <code>{children}</code>;
-  }
-  if (leaf.italic) {
-    children = <em>{children}</em>;
-  }
-  if (leaf.underline) {
-    children = <u>{children}</u>;
-  }
-  return <span {...attributes}>{children}</span>;
-};
