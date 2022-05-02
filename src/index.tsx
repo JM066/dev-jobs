366319f955d9f46421c4d226076b297129596387
Jungmin Lee <mina.lee066@gmail.com>
2022-05-02 18:10:31 +0900

    SSR in progress

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ const Root = () => {
 };
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Root />, rootElement);
+ReactDOM.hydrate(<Root />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
